e355daac0b1b2538faeb67404b64bdcb8226722f
Muneeb jawed <muneebjawed304@gmail.com>
2024-11-29 16:42:09 +0500

    size changes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,7 +10,7 @@ const Skills = () => {
                 <p className='text-gray-400 pt-2'>I am a passionate web developer with expertise in crafting responsive, user-friendly websites and web applications. Proficient in front-end technologies such as React, Next.js, Tailwind CSS, HTML, CSS, and JavaScript, along with TypeScript, I specialize in building dynamic and intuitive interfaces that enhance user experiences. My commitment to clean, efficient code ensures seamless performance and scalability. Whether working on independent projects or collaborating with teams, I strive to deliver solutions that align with client goals and modern web standards.</p>
             </div>
             <div>
-                <div className='grid grid-cols-2 text-accent text-4xl sm:text-3xl'>
+                <div className='grid grid-cols-2 sm:gap-6 text-accent text-4xl sm:text-3xl'>
                     <div className='space-y-4'>
                         <h2 data-aos="fade-up"
      data-aos-duration="3000">React.js</h2>
